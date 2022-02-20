9208e118576db36b66e1edebe8a05a9eed4c68a8
Takatugu-work <takatuguwork@gmail.com>
2022-02-20 16:42:43 +0900

    test

---
+++ components/navbar.tsx
@@ -0,0 +1,30 @@
+import styles from "../styles/navbar.module.scss";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className={styles.nav_wrapper}>
+      <div className={styles.nav_container}>
+        <div className={styles.main_title}>
+          <p>
+            <Link href="/">かわいいポケモンずかん</Link>
+          </p>
+        </div>
+        <div className={styles.nav_menu}>
+          <ul className={styles.ul}>
+            <li>
+              <Link href="/">マイページ</Link>
+            </li>
+            <li>
+              <Link href="/">人気のポケモン</Link>
+            </li>
+            <li>
+              <Link href="/">このサイトについて</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
