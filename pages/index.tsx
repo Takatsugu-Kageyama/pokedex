eb054257b4d956b83196549dd201a9093a7909b3
Takatugu-work <takatuguwork@gmail.com>
2022-02-20 18:25:26 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Login from "../components/login";
 
 const Home: NextPage = () => {
   return (
-    <Head>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
     (function(d) {
     var config = {
       kitId: 'mlv6mbl',
@@ -17,10 +19,12 @@ const Home: NextPage = () => {
     h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
   })(document);
   `,
-        }}
-      />
-      <title>かわいいポケモン図鑑</title>
-    </Head>
+          }}
+        />
+        <title>かわいいポケモン図鑑</title>
+      </Head>
+      <Login />
+    </>
   );
 };
 
