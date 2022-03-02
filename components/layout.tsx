09f9586fe246f4287ba4542ff891b9fe5a0d1f49
Takatugu-windwos <takatugu912@ezweb.ne.jp>
2022-03-02 12:03:42 +0900

    test

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,11 +5,13 @@ import styles from "../styles/layout.module.scss";
 
 const Layout: NextPage = ({ children }) => {
   return (
-    <div className={styles.overall}>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
-    </div>
+    <>
+      <div className={styles.overall}>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </>
   );
 };
 export default Layout;
